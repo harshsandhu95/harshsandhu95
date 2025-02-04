b24c724c2adc369ded536abbda5e21e784e7136a
Harsh Sandhu <nyxfor13days@gmail.com>
2025-02-04 16:59:23 +0000

    Refactor layout and home page to use dynamic metadata and simplify structure

---
+++ src/lib/config.ts
@@ -0,0 +1,7 @@
+export const data = {
+  title: "Harsh Sandhu - Frontend Developer",
+  description: "A Frontend Developer born and raised in India. I am passionate about building excellent web and mobile apps.",
+  full_name: "Harsh Sandhu",
+  first_name: "Harsh",
+  last_name: "Sandhu",
+};
