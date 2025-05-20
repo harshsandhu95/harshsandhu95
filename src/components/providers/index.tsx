cdbe971208a75d68bae1c58a9fad21bfb1c49bb2
Harsh Sandhu <me@harshsandhu.com>
2025-05-21 00:20:02 +0530

    feat: Introduce ThemeProvider to RootProvider for enhanced theming support

--- src/components/providers/index.tsx
+++ src/components/providers/index.tsx
@@ -1,9 +1,15 @@
+import ThemeProvider from "./theme-provider";
+
 interface RootProviderProps {
   children: React.ReactNode;
 }
 
 const RootProvider: React.FC<RootProviderProps> = ({ children }) => {
-  return <>{children}</>;
+  return (
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      {children}
+    </ThemeProvider>
+  );
 };
 
 export default RootProvider;
---
+++ src/components/providers/theme-provider.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import * as React from "react";
+import {
+  ThemeProvider as NextThemesProvider,
+  ThemeProviderProps,
+} from "next-themes";
+
+const ThemeProvider: React.FC<ThemeProviderProps> = ({
+  children,
+  ...props
+}) => {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+};
+
+export default ThemeProvider;
